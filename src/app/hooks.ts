ef71ccd9593643a9d9804e4259e141d48837763e
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2024-08-13 11:26:31 +0000

    elimine tracking hasta implementar Ackee

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -6,18 +6,18 @@ import { useSingleton } from '@/common/hooks'
 const DETAILED = true
 
 export const useAckee = () => {
-  const instance = useSingleton(() => tracker.create('https://ackee.exuanbo.xyz/'))
+  // const instance = useSingleton(() => tracker.create('https://ackee.exuanbo.xyz/'))
 
   useEffect(() => {
-    const attributes = tracker.attributes(DETAILED)
+    // const attributes = tracker.attributes(DETAILED)
 
-    const { pathname, origin } = window.location
-    const url = new URL(pathname, origin)
+    // const { pathname, origin } = window.location
+    // const url = new URL(pathname, origin)
 
-    const { stop } = instance.record('bc75fd47-884f-4723-aaf6-3384103e0095', {
-      ...attributes,
-      siteLocation: url.href,
-    })
-    return stop
-  }, [instance])
+    // const { stop } = instance.record('bc75fd47-884f-4723-aaf6-3384103e0095', {
+    //   ...attributes,
+    //   siteLocation: url.href,
+    // })
+    // return stop
+  }, [])
 }
